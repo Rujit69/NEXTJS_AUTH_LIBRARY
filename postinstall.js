523f1f91960d0d78c46c6fb7636f4e966eb4357f
Rujit33 <rujit33@gmail.com>
2025-05-31 22:27:29 +0545

    post install hook

--- postinstall.js
+++ postinstall.js
@@ -1,3 +1,12 @@
 // postinstall.js
-console.log('\n🎉 Thanks for installing rujit_form!');
-console.log('Run `npx rujit_auth` or `rujit_auth` command to create files and folders in your project.\n');
+
+// postinstall.js
+const fs = require("fs");
+fs.writeFileSync(
+  "RUJIT_FORM_INSTALL.txt",
+  "Thanks for installing rujit_form!\nRun `npx rujit_form`..."
+);
+console.error("\n🎉 Thanks for installing rujit_form!");
+console.error(
+  "Run `npx rujit_form` or `rujit_form` command to create files and folders in your project.\n"
+);
