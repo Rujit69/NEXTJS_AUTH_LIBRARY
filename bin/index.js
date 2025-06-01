016173269a31d9adcedc87a208f55b8049e475a2
Rujit33 <rujit33@gmail.com>
2025-06-01 10:19:08 +0545

    npx create

--- bin/index.js
+++ bin/index.js
@@ -1,20 +1,28 @@
 #!/usr/bin/env node
-
 const path = require("path");
 const fs = require("fs-extra");
+const { execSync } = require("child_process");
 
 const cwd = process.cwd();
 const templateDir = path.join(__dirname, "../template");
 
-async function copyTemplate() {
+async function setupProject() {
   try {
     console.log("📦 Copying rujit_form template files...");
 
+    // Copy template files to the current working directory
     await fs.copy(templateDir, cwd, { overwrite: true });
     console.log("✅ rujit_form setup complete!");
+
+    console.log("📦 Installing required dependencies...");
+    // Install required dependencies in the current project.
+    execSync("npm install next-auth fs-extra mongoose bcryptjs", {
+      stdio: "inherit",
+    });
+    console.log("✅ Dependencies installed!");
   } catch (err) {
-    console.error("❌ Failed to copy files:", err);
+    console.error("❌ Failed to setup project:", err);
   }
 }
 
-copyTemplate();
+setupProject();
--- postinstall.js
+++ postinstall.js
@@ -1,12 +1,5 @@
 // postinstall.js
-
-// postinstall.js
-const fs = require("fs");
-fs.writeFileSync(
-  "RUJIT_FORM_INSTALL.txt",
-  "Thanks for installing rujit_form!\nRun `npx rujit_form`..."
-);
-console.error("\n🎉 Thanks for installing rujit_form!");
-console.error(
+console.log("\n🎉 Thanks for installing rujit_form!");
+console.log(
   "Run `npx rujit_form` or `rujit_form` command to create files and folders in your project.\n"
 );
